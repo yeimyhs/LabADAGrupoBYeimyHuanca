5182f16106cfc0d631eaa18c583d8faa1e004deb
yeimyhs <67721046+yeimyhs@users.noreply.github.com>
2021-11-12 16:37:42 -0500

    Ejercicio 2: Complejidad On (For)

---
+++ FifthClass/12excercise/excercise2.py
@@ -0,0 +1,27 @@
+##Ejercicio 2
+# Cual es la complejidad del codigo?
+# 
+# Entrada:
+# -
+# Salida:
+'''
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+'''
+# Lo que evidencia qque la complejidad de ese for es de n
+# O(n)
+#
+# Autor:  Yeimy Huanca
+  
+n=10
+for i in range(n,0,-1):
+    print (i)
+
